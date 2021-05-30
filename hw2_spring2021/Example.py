dd91ebf182b6bb217b0d94ffcace03a3773ba65f
Yair Davidson <yair.davidson1@gmail.com>
2021-05-30 17:58:30 +0300

    added first func

--- hw2_spring2021/Example.py
+++ hw2_spring2021/Example.py
@@ -2,6 +2,7 @@ import Utility.DBConnector as Connector
 from Utility.ReturnValue import ReturnValue
 from Utility.Exceptions import DatabaseException
 from Utility.DBConnector import ResultSet
+from Business.Query import Query
 from psycopg2 import sql
 
 
@@ -9,7 +10,8 @@ def dropTable() -> None:
     conn = None
     try:
         conn = Connector.DBConnector()
-        conn.execute("DROP TABLE IF EXISTS Users CASCADE")
+        conn.execute("DROP TABLE IF EXISTS Users,Query, Disk, Ram, QueryOnDisk, RamOnDisk CASCADE")
+        conn.execute("DROP VIEW IF EXISTS ViewDiskAndQuery, ViewDiskOnQuery CASCADE")
         conn.commit()
     except DatabaseException.ConnectionInvalid as e:
         # do stuff
@@ -42,10 +44,65 @@ def createTable() -> None:
                      "(\n" +
                      "    id integer,\n" +
                      "    purpose text NOT NULL ,\n" +
-                     "    disk_size_needed text NOT NULL ,\n" +
+                     "    disk_size_needed integer NOT NULL ,\n" +
                      "    PRIMARY KEY (id),\n" +
-                     "    CHECK (id > 0)\n" +
+                     "    CHECK (id > 0),\n" +
+                     "    CHECK (disk_size_needed >= 0)\n" +
                      ")")
+
+        conn.execute("CREATE TABLE Disk\n" +
+                     "(\n" +
+                     "    id integer,\n" +
+                     "    manufacturing_company text NOT NULL ,\n" +
+                     "    speed integer NOT NULL ,\n" +
+                     "    free_space integer NOT NULL ,\n" +
+                     "    cost_per_byte integer NOT NULL ,\n" +
+                     "    PRIMARY KEY (id),\n" +
+                     "    CHECK (id > 0),\n" +
+                     "    CHECK (speed > 0),\n" +
+                     "    CHECK (cost_per_byte > 0),\n" +
+                     "    CHECK (free_space >= 0)\n" +
+                     ")")
+
+        conn.execute("CREATE TABLE Ram\n" +
+                     "(\n" +
+                     "    id integer,\n" +
+                     "    size integer NOT NULL ,\n" +
+                     "    company text NOT NULL ,\n" +
+                     "    PRIMARY KEY (id),\n" +
+                     "    CHECK (id > 0),\n" +
+                     "    CHECK (size > 0)\n" +
+                     ")")
+        
+        conn.execute("CREATE TABLE QueryOnDisk\n" +
+                     "(\n" +
+                     "    query_id integer,\n" +
+                     "    disk_id integer,\n"
+                     "    PRIMARY KEY (query_id, disk_id),\n" +
+                     "    FOREIGN KEY (query_id) REFERENCES Query(id) ON DELETE CASCADE,\n" +
+                     "    FOREIGN KEY (disk_id) REFERENCES Disk(id) ON DELETE CASCADE\n" +
+                     ")")
+        
+        conn.execute("CREATE TABLE RamOnDisk\n" +
+                     "(\n" +
+                     "    ram_id integer,\n" +
+                     "    disk_id integer,\n"
+                     "    PRIMARY KEY (ram_id, disk_id),\n" +
+                     "    FOREIGN KEY (ram_id) REFERENCES Ram(id) ON DELETE CASCADE,\n" +
+                     "    FOREIGN KEY (disk_id) REFERENCES Disk(id) ON DELETE CASCADE\n" +
+                     ")")
+        
+        conn.execute("CREATE VIEW ViewDiskAndQuery AS\n" +
+                     "SELECT q.id AS query_id, q.disk_size_needed," +
+                     "d.id AS disk_id, d.free_space" +
+                     " FROM Query q, Disk d\n" )
+
+        conn.execute("CREATE VIEW ViewDiskOnQuery AS\n" +
+                     "SELECT qod.query_id , qod.disk_id," +
+                     "d.free_space, q.disk_size_needed" +
+                     " FROM Query q, Disk d, QueryOnDisk as qod" +
+                     "\tWHERE d.id = qod.disk_id AND q.id = qod.disk_id;\n")
+
         conn.commit()
     except DatabaseException.ConnectionInvalid as e:
         print(e)
@@ -112,10 +169,43 @@ def addUser(ID: int, name: str) -> ReturnValue:
     finally:
         conn.close()
         return ReturnValue.OK
+def addQuery(query: Query) -> ReturnValue:
+    conn = None
+    try:
+        conn = Connector.DBConnector()
+        id_param = query.getQueryID()
+        purpose_param = query.getPurpose()
+        size_param = query.getSize()
+        q = sql.SQL("INSERT INTO Query(id, purpose, disk_size_needed) VALUES({id}, {purpose}, {disk_size})").format(id=sql.Literal(id_param), purpose=sql.Literal(purpose_param), disk_size=sql.Literal(size_param))
+        conn.execute(q)
+        conn.commit()
+    except DatabaseException.ConnectionInvalid as e:
+        # do stuff
+        print(e)
+    except DatabaseException.NOT_NULL_VIOLATION as e:
+        # do stuff
+        print(e)
+    except DatabaseException.CHECK_VIOLATION as e:
+        # do stuff
+        print(e)
+    except DatabaseException.UNIQUE_VIOLATION as e:
+        # do stuff
+        print(e)
+    except DatabaseException.FOREIGN_KEY_VIOLATION as e:
+        # do stuff
+        print(e)
+    except Exception as e:
+        print(e)
+    finally:
+        # will happen any way after code try termination or exception handling
+        conn.close()
+    return ReturnValue.OK
+
 
 
 def deleteUser(ID: int, persistent: bool = True) -> int:
     conn = None
+    rows_effected = 0
     rows_effected = 0
     try:
         conn = Connector.DBConnector()
@@ -180,5 +270,8 @@ if __name__ == '__main__':
         current_row = users[index]  # get the row
         for col in current_row:  # iterate over the columns
             print(str(col) + "=" + str(current_row[col]))
+    print("10.5. testing add query")
+    query = Query(1, "test", 10)
+    addQuery(query)
     print("11. Dropping all tables - empty database")
     dropTable()
--- hw2_spring2021/Solution.py
+++ hw2_spring2021/Solution.py
@@ -8,37 +8,102 @@ from Business.Disk import Disk
 from psycopg2 import sql
 
 
-def createTables():
+def errorHandler(e):
+    ret_val = ReturnValue.ERROR
+    if isinstance(e,DatabaseException.NOT_NULL_VIOLATION):
+        ret_val = ReturnValue.BAD_PARAMS
+    elif isinstance(e,DatabaseException.CHECK_VIOLATION):
+        ret_val = ReturnValue.BAD_PARAMS
+    elif isinstance(e,DatabaseException.UNIQUE_VIOLATION):
+        ret_val = ReturnValue.ALREADY_EXISTS
+    return ret_val
+
+
+
+
+def createTable() -> None:
     conn = None
     try:
         conn = Connector.DBConnector()
-        conn.execute("CREATE TABLE Employee\n" +
-                      "(\n" +
-                      "    id integer,\n" +
-                      "    Name text NOT NULL ,\n" +
-                      "    City_of_Birth text NOT NULL ,\n" +
-                      "    PRIMARY KEY (id),\n" +
-                      "    CHECK (id > 0)\n" +
-                      ")")
-        except DatabaseException.ConnectionInvalid as e:
-        # do stuff
-        print(e)
+        conn.execute("CREATE TABLE Users(id INTEGER PRIMARY KEY, name TEXT NOT NULL)")
+        conn.execute("CREATE TABLE Query\n" +
+                     "(\n" +
+                     "    id integer,\n" +
+                     "    purpose text NOT NULL ,\n" +
+                     "    disk_size_needed integer NOT NULL ,\n" +
+                     "    PRIMARY KEY (id),\n" +
+                     "    CHECK (id > 0),\n" +
+                     "    CHECK (disk_size_needed >= 0)\n" +
+                     ")")
+
+        conn.execute("CREATE TABLE Disk\n" +
+                     "(\n" +
+                     "    id integer,\n" +
+                     "    manufacturing_company text NOT NULL ,\n" +
+                     "    speed integer NOT NULL ,\n" +
+                     "    free_space integer NOT NULL ,\n" +
+                     "    cost_per_byte integer NOT NULL ,\n" +
+                     "    PRIMARY KEY (id),\n" +
+                     "    CHECK (id > 0),\n" +
+                     "    CHECK (speed > 0),\n" +
+                     "    CHECK (cost_per_byte > 0),\n" +
+                     "    CHECK (free_space >= 0)\n" +
+                     ")")
+
+        conn.execute("CREATE TABLE Ram\n" +
+                     "(\n" +
+                     "    id integer,\n" +
+                     "    size integer NOT NULL ,\n" +
+                     "    company text NOT NULL ,\n" +
+                     "    PRIMARY KEY (id),\n" +
+                     "    CHECK (id > 0),\n" +
+                     "    CHECK (size > 0)\n" +
+                     ")")
+
+        conn.execute("CREATE TABLE QueryOnDisk\n" +
+                     "(\n" +
+                     "    query_id integer,\n" +
+                     "    disk_id integer,\n"
+                     "    PRIMARY KEY (query_id, disk_id),\n" +
+                     "    FOREIGN KEY (query_id) REFERENCES Query(id) ON DELETE CASCADE,\n" +
+                     "    FOREIGN KEY (disk_id) REFERENCES Disk(id) ON DELETE CASCADE\n" +
+                     ")")
+
+        conn.execute("CREATE TABLE RamOnDisk\n" +
+                     "(\n" +
+                     "    ram_id integer,\n" +
+                     "    disk_id integer,\n"
+                     "    PRIMARY KEY (ram_id, disk_id),\n" +
+                     "    FOREIGN KEY (ram_id) REFERENCES Ram(id) ON DELETE CASCADE,\n" +
+                     "    FOREIGN KEY (disk_id) REFERENCES Disk(id) ON DELETE CASCADE\n" +
+                     ")")
+
+        conn.execute("CREATE VIEW ViewDiskAndQuery AS\n" +
+                     "SELECT q.id AS query_id, q.disk_size_needed," +
+                     "d.id AS disk_id, d.free_space" +
+                     " FROM Query q, Disk d\n")
+
+        conn.execute("CREATE VIEW ViewDiskOnQuery AS\n" +
+                     "SELECT qod.query_id , qod.disk_id," +
+                     "d.free_space, q.disk_size_needed" +
+                     " FROM Query q, Disk d, QueryOnDisk as qod" +
+                     "\tWHERE d.id = qod.disk_id AND q.id = qod.disk_id;\n")
+
+        conn.commit()
+    except DatabaseException.ConnectionInvalid as e:
+        conn.rollbak()
     except DatabaseException.NOT_NULL_VIOLATION as e:
-        # do stuff
-        print(e)
+        conn.rollbak()
     except DatabaseException.CHECK_VIOLATION as e:
-        # do stuff
-        print(e)
+        conn.rollbak()
     except DatabaseException.UNIQUE_VIOLATION as e:
-        # do stuff
-        print(e)
+        conn.rollbak()
     except DatabaseException.FOREIGN_KEY_VIOLATION as e:
-        # do stuff
-        print(e)
+        conn.rollbak()
     except Exception as e:
-        print(e)
+        conn.rollbak()
     finally:
-        # will happen any way after code try termination or exception handling
+        # will happen any way after try termination or exception handling
         conn.close()
 
 
@@ -47,11 +112,52 @@ def clearTables():
 
 
 def dropTables():
-    pass
+    conn = None
+    try:
+        conn = Connector.DBConnector()
+        conn.execute("DROP TABLE IF EXISTS Users,Query, Disk, Ram, QueryOnDisk, RamOnDisk CASCADE")
+        conn.execute("DROP VIEW IF EXISTS ViewDiskAndQuery, ViewDiskOnQuery CASCADE")
+        conn.commit()
+    except DatabaseException.ConnectionInvalid as e:
+        # do stuff
+        conn.rollback()
+    except DatabaseException.NOT_NULL_VIOLATION as e:
+        # do stuff
+        conn.rollback()
+    except DatabaseException.CHECK_VIOLATION as e:
+        # do stuff
+        conn.rollback()
+    except DatabaseException.UNIQUE_VIOLATION as e:
+        # do stuff
+        conn.rollback()
+    except DatabaseException.FOREIGN_KEY_VIOLATION as e:
+        # do stuff
+        conn.rollback()
+    except Exception as e:
+        conn.rollback()
+    finally:
+        # will happen any way after code try termination or exception handling
+        conn.close()
 
 
 def addQuery(query: Query) -> ReturnValue:
-    return ReturnValue.OK
+    conn = None
+    ret_val = ReturnValue.OK
+    try:
+        conn = Connector.DBConnector()
+        id_param = query.getQueryID()
+        purpose_param = query.getPurpose()
+        size_param = query.getSize()
+        q = sql.SQL("INSERT INTO Query(id, purpose, disk_size_needed) VALUES({id}, {purpose}, {disk_size})").format(id=sql.Literal(id_param), purpose=sql.Literal(purpose_param), disk_size=sql.Literal(size_param))
+        conn.execute(q)
+        conn.commit()
+    except Exception as e:
+        ret_val = errorHandler(e)
+        conn.rollback()
+    finally:
+        # will happen any way after code try termination or exception handling
+        conn.close()
+    return ret_val
 
 
 def getQueryProfile(queryID: int) -> Query:
